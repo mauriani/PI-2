09f06923d369ffec51603a920de60a4887e6b37d
mauriani <maurianim@gmail.com>
2021-08-31 14:39:09 -0300

    atrs

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
 
 import {
   Container,
@@ -15,40 +15,64 @@ import {
 import Header from '../../components/Header';
 
 export default function Dashboard() {
+  const data = [
+    {
+      id: '1',
+      patientName: 'Maria Luiza Rodrigues',
+      hour: '16:00',
+    },
+    {
+      id: '2',
+      patientName: 'Luiz Fernando De Paula',
+      hour: '17:00',
+    },
+    {
+      id: '3',
+      patientName: 'João Paulo Loureiro',
+      hour: '18:00',
+    },
+    {
+      id: '4',
+      patientName: 'João Paulo Loureiro',
+      hour: '18:00',
+    },
+    {
+      id: '5',
+      patientName: 'Maria Luiza',
+      hour: '18:00',
+    },
+    {
+      id: '6',
+      patientName: 'Pedro Antônio',
+      hour: '18:00',
+    },
+    {
+      id: '7',
+      patientName: 'João Paulo Loureiro',
+      hour: '18:00',
+    },
+  ];
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <ScrollView
-          vertical={true}
+        <FlatList
+          data={data}
+          keyExtractor={item => item.id}
           showsVerticalScrollIndicator={false}
           style={{ marginBottom: 10 }}
-        >
-          <Card>
-            <Title>Ana Maria Rodrigues Silva</Title>
-            <ContainerHour>
-              <SubTitle>12:00</SubTitle>
-              <Icon name="bell" />
-            </ContainerHour>
-          </Card>
-
-          <Card>
-            <Title>Antônio Carlos Silva</Title>
-            <ContainerHour>
-              <SubTitle>13:00</SubTitle>
-              <Icon name="bell" />
-            </ContainerHour>
-          </Card>
-
-          <Card>
-            <Title>Mariana De Almeida</Title>
-            <ContainerHour>
-              <SubTitle>14:00</SubTitle>
-              <Icon name="bell" />
-            </ContainerHour>
-          </Card>
-        </ScrollView>
+          renderItem={({ item }) => (
+            <Card>
+              <Title>{item.patientName}</Title>
+              <ContainerHour>
+                <SubTitle>{item.hour}</SubTitle>
+                <Icon name="bell" />
+              </ContainerHour>
+            </Card>
+          )}
+        />
       </Content>
     </Container>
   );
