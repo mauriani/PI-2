4fcd6601b5183ccf56acc6e136a0642122111959
mauriani <maurianim@gmail.com>
2021-09-01 11:27:29 -0300

    alteracão em navegação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Title, Form, SignUpButton, ButtonText } from './styles';
 
 export default function SignUp() {
+  const { navigate } = useNavigation();
+
+  const navigateToSignIn = useCallback(() => {
+    navigate('SignIn');
+  }, [navigate]);
+
   return (
     <Container>
       <Form>
@@ -15,9 +21,9 @@ export default function SignUp() {
         <Input placeholder="E-mail" placeholderTextColor="#3D3D4C" />
         <Input placeholder="Digite uma Senha" placeholderTextColor="#3D3D4C" />
 
-        <Button title="Login" onPress={() => {}} />
+        <Button title="Cadastrar-se" onPress={() => {}} />
 
-        <SignUpButton onPress={() => {}}>
+        <SignUpButton onPress={navigateToSignIn}>
           <ButtonText>Voltar para login</ButtonText>
         </SignUpButton>
       </Form>
