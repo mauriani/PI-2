b289ab45d1e6411f5f567406126206085ae99121
mauriani <maurianim@gmail.com>
2021-10-13 12:15:06 -0300

    attrs

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,26 @@
 import React, { useCallback } from 'react';
 import { StatusBar } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import { Container, Topo, Image, LogoutButton, Icon } from './styles';
 
-import firebase from '../../config/firebaseconfig';
 import logo from '../../assets/images/medic04_branco.png';
 
 export default function Header() {
   const { navigate } = useNavigation();
 
   const handleLogOut = useCallback(() => {
-    firebase
-      .auth()
+    auth()
       .signOut()
       .then(() => {
+        const dataKey = '@medic:user';
+        AsyncStorage.removeItem(dataKey);
         navigate('SignIn');
       })
-      .catch(error => {});
+      .catch(error => {
+        
+      });
   });
 
   return (
